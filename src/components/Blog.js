f8d2df58296158cfa20567e1763090819416ebfa
unknown <you@example.com>
2024-05-17 19:19:34 +0400

    save

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,6 +4,7 @@ import "./search.css";
 import NewsCard from "./NewsCard";
 import Button from "./Button";
 import Section from "./Section";
+
 function Blog() {
   let tags = [
     "american",
@@ -17,14 +18,24 @@ function Blog() {
     "english",
     "magical",
   ];
+
   const [data, setData] = useState([]);
   const [text, setText] = useState("");
   const [timerId, setTimerId] = useState();
   const [categories, setCategories] = useState([]);
-  const [buttonColor, setButtonColor] = useState('#69bad3');
+  const [reset, setReset] = useState(false);
+  const [buttonColors, setButtonColors] = useState(tags.map(() => '#69bad3'));
+
   let arr = data;
 
-  let arr2 = [];
+  let btnStyle = {
+    borderRadius:'20px',
+    color:'#fff',
+    margin:'5px',
+    width:'70px',
+    height:'30px', 
+  };
+
   useEffect(() => {
     setTimerId(
       setTimeout(() => {
@@ -44,6 +55,7 @@ function Blog() {
       }, 1000)
     );
   }, [text]);
+
   useEffect(() => {
     clearTimeout(timerId);
     fetch("https://dummyjson.com/posts")
@@ -57,26 +69,25 @@ function Blog() {
       );
     console.log(categories, data);
   }, [categories]);
-  function colorChange(btn){
-	if (btn.style.backgroundColor == '#69bad3') {
-			setButtonColor('red')
-		}else{
-			setButtonColor('#69bad3')
-		}
-	}
-  
-  
+
+
+
+  const handleReset = () => {
+	fetch("https://dummyjson.com/posts")
+	.then((res) => res.json())
+	.then((res) => setData(res.posts));
+    setButtonColors(tags.map(() => '#69bad3'));
+  };
+
   return (
     <>
-      <section className=" flex gap-12  justify-between p-60 ">
-        <div className=" flex gap-5 flex-col">
-          <h1 className=" z-20 text-6xl  text-center">Blog posts</h1>
-          <div className=" gap-10 flex flex-wrap justify-center items-center">
-            {arr.map((e, i) => {
-              return (
-                <NewsCard tags={e.tags} key={i} title={e.title} text={e.body} />
-              );
-            })}
+      <section className="flex gap-12 justify-between p-60">
+        <div className="flex gap-5 flex-col">
+          <h1 className="z-20 text-6xl text-center">Blog posts</h1>
+          <div className="gap-10 flex flex-wrap justify-center items-center">
+            {arr.map((e, i) => (
+              <NewsCard tags={e.tags} key={i} title={e.title} text={e.body} />
+            ))}
           </div>
         </div>
         <div>
@@ -90,40 +101,36 @@ function Blog() {
                   placeholder="Search"
                 />
               </div>
-              <div className=" p-10 gap-10">
-                {tags.map((e, i) => {
-                  return (
-                    <button
-					style={backgroundColor=`${buttonColor}`}
-                      key={i}
-					  className="categoriesBtn"
-                      onClick={() => {
-                        setCategories(e);
-						colorChange(this)
-                      }}
-                    >
-                      {e}
-                    </button>
-                  );
-                })}
+              <div className="p-10 gap-10">
+                {tags.map((e, i) => (
+                  <button
+                    style={{ ...btnStyle, background: buttonColors[i] }}
+                    key={i}
+                    className="categoriesBtn"
+                    onClick={() => {
+                      setCategories(e);
+                      setButtonColors(prevColors => {
+                        const newColors = [...prevColors];
+                        newColors[i] = newColors[i] === '#69bad3' ? 'red' : '#69bad3';
+                        return newColors;
+                      });
+                    }}
+                  >
+                    {e}
+                  </button>
+                ))}
+                <button className="categBtn" onClick={handleReset}>X</button>
               </div>
             </div>
           </div>
         </div>
       </section>
       <Section />
-      <div className=" w-full h-1"></div>
+      <div className="w-full h-1"></div>
     </>
-	
   );
 }
 
 export default Blog;
-//american,love,fiction,mystery,crime,classic,french,history,english,magical
-// <Button
-// padding='10px'
-//   text={e}
-//   back="#7772F1"
-//   color="white"
-//   efect={true}
-// ></Button>
+
+
